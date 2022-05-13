684eb9e7f17e0de4c6ffe41edc474084f56d1f9d
SianTharit <Tharit.preecha1@gmail.com>
2022-05-13 13:34:48 +0700

    sequelize-cli

---
+++ index.js
@@ -0,0 +1,18 @@
+// const db = require("./models");
+// db.sequelize.authenticate().then(() => console.log("DB connected"));
+
+const { sequelize, User } = require("./models");
+sequelize.sync({ force: true });
+
+// const run = async () => {
+//     try {
+//         const user = await User.create({
+//             username: "John",
+//             password: "123456",
+//             email: "JOHN@gmail.com",
+//         });
+//     } catch (err) {
+//         console.log(err);
+//     }
+// };
+// run();
